83b91dd94658d9399d45b15d8f4631403f4a322a
arthurfranca <arthur.a.franca@gmail.com>
2013-11-12 21:20:38 -0200

    adiciona estrutura inicial - rails 4 e mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Psw::Application.config.secret_key_base = '4da402089bb69a9d7c2b2f331b30ae14ceb5f0473f87d3d39acc1e5e08c2963c6121f11041a6b2cd6bc71610546c1144b27d479728ee6df39538d10668c35c83'
